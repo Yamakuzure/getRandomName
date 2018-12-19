0ad55c7f6e618de5a367e6da2c566fc69d09b789
Sven Eden <sven.eden@prydeworx.com>
2018-12-19 14:29:51 +0100

    Complete the licensing information

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,30 @@
   * This is the main file for getRandomName, a small tool to create random names
   * utilizing RNG of the pwxLib.
   *
-  * (c) 2007-2012 Sven Eden, PrydeWorX
-  * @author Sven Eden, PrydeWorX - Hamburg, Germany
-  *         yamakuzure@users.sourceforge.net
-  *         http://pwxlib.sourceforge.net
+  * (c) 2007 - 2018 PrydeWorX
+  * @author Sven Eden, PrydeWorX - Bardowick, Germany
+  *         sven.eden@prydeworx.com
+  *         https://github.com/Yamakuzure/getRandomName ; https://prydeworx.com/getRandomName
   *
-  *  This program is free software: you can redistribute it and/or modify
-  *  it under the terms of the GNU General Public License as published by
-  *  the Free Software Foundation, either version 3 of the License, or
-  *  (at your option) any later version.
-  *
-  *  This program is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU General Public License for more details.
-  *
-  *  You should have received a copy of the GNU General Public License
-  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * getRandomName is free software under MIT License
+  * 
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  * 
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  * 
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
   *
   @verbatim
   * History and Changelog:
@@ -44,7 +51,8 @@
   * 0.8.5.1   2012-03-20  sed, PrydeWorX  Fixed a memory leak introduced with version 0.7.0.2
   * 0.8.5.2   2012-03-23  sed, PrydeWorX  Fixed an Effective C++ issue
   * 0.8.6     2012-04-??  sed, PrydeWorX  Version bump to new pwxLib release version
-  * 0.9.0     2018-12-19  sed, PrydeWorX  Updated to support the latest pwxLib version
+  * 0.9.0     2018-12-19  sed, PrydeWorX  Updated to support the latest pwxLib version and fixed
+  *                                       a possible memory leak when writing into an output file.
   @endverbatim
 **/
 
--- src/main.h
+++ src/main.h
@@ -1,6 +1,39 @@
 #ifndef PWX_GETRANDOMNAME_MAIN_H
 #define PWX_GETRANDOMNAME_MAIN_H 1
 
+/** @file main.h
+ * 
+ * This file is part of getRandomName
+ *
+ * (c) 2007 - 2018 PrydeWorX
+ * @author Sven Eden, PrydeWorX - Bardowick, Germany
+ *         sven.eden@prydeworx.com
+ *         https://github.com/Yamakuzure/getRandomName ; https://prydeworx.com/getRandomName
+ *
+ * getRandomName is free software under MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * History and change log are maintained in main.cpp
+ **/
+
+
 #include <cstdlib>
 #include <climits>
 #include <cstring>
